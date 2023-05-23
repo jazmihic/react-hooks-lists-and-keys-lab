33e2fffe923e08093566c9170f0ed39ab276a109
Jazmine <jazmihic@amazon.com>
2023-05-23 13:01:19 -0500

    changes made

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linksElements = links.map((links) => {
+    return <a key={links} href={"#" + links}>{links}</a>
+  })
+
+  return <nav>{ linksElements }</nav>;
 }
 
 export default NavBar;
